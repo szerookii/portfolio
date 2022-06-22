cee7dfe28139c23a78c49b1a0e01f290aba33479
szeroki <36236750+Seyz123@users.noreply.github.com>
2022-06-22 13:05:09 +0200

    Remove magicgrid plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,6 +60,6 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['vue-magic-grid', 'vue-fontawesome'],
+    transpile: ['vue-fontawesome'],
   }
 }
--- plugins/magicgrid.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import MagicGrid from 'vue-magic-grid'
-
-Vue.use(MagicGrid)
