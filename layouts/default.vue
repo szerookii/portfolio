bcb79ab3a02442585969a0f8bfe8461bceccdc7e
szeroki <36236750+Seyz123@users.noreply.github.com>
2022-06-22 15:58:50 +0200

    Move global page headers in default layout

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  head() {
+    return {
+      title: 'szeroki - Portfolio',
+      htmlAttrs: {
+        lang: 'fr'
+      },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'og:title', name: 'og:title', content: 'szeroki' },
+        { hid: 'description', name: 'description', content: 'szeroki, backend developer and reverse engineer.' },
+        { hid: 'og:description', name: 'og:description', content: 'szeroki, backend developer and reverse engineer.' },
+        { hid: 'og:image', name: 'og:image', content: 'https://avatars.githubusercontent.com/u/36236750?v=4' },
+        { hid: 'theme-color', name: 'theme-color', content: '#E7A854' },
+        { name: 'format-detection', content: 'telephone=no' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico' }
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <Nuxt />
+</template>
--- nuxt.config.js
+++ nuxt.config.js
@@ -3,27 +3,6 @@ export default {
     port: process.env.PRODUCTION ? 80 : 3000
   },
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'szeroki - Portfolio',
-    htmlAttrs: {
-      lang: 'fr'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'og:title', name: 'og:title', content: 'szeroki' },
-      { hid: 'description', name: 'description', content: 'szeroki, backend developer and reverse engineer.' },
-      { hid: 'og:description', name: 'og:description', content: 'szeroki, backend developer and reverse engineer.' },
-      { hid: 'og:image', name: 'og:image', content: 'https://avatars.githubusercontent.com/u/36236750?v=4' },
-      { hid: 'theme-color', name: 'theme-color', content: '#E7A854' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/img/favicon.ico' }
-    ]
-  },
-
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
     "~/static/css/main.scss",
