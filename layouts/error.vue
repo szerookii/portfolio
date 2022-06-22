9ad98f31c1e454bf26e011532cf7117083e6e84c
szeroki <36236750+Seyz123@users.noreply.github.com>
2022-06-22 14:13:00 +0200

    indent

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,33 +1,33 @@
 <script>
 export default {
-    props: ["error"]
+  props: ["error"]
 }
 </script>
 
 <template>
-    <div>
-        <Loader />
-        <div class="error-container">
-            <h1 v-if="error.statusCode === 404">Page not found</h1>
-            <h1 v-else>An error occurred</h1>
-            <NuxtLink class="link" to="/"><p>Back to home</p></NuxtLink>
-        </div>
+  <div>
+    <Loader />
+    <div class="error-container">
+      <h1 v-if="error.statusCode === 404">Page not found</h1>
+      <h1 v-else>An error occurred</h1>
+      <NuxtLink class="link" to="/">
+        <p>Back to home</p>
+      </NuxtLink>
     </div>
+  </div>
 </template>
 
 <style lang="scss">
 .error-container {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    
+  position: absolute;
+  top: 50%;
+  left: 50%;
+
+  text-align: center;
+  transform: translate(-50%, -50%);
+
+  h1 {
     text-align: center;
-    transform: translate(-50%, -50%);
-
-    h1 {
-        text-align: center;
-    }
-
-    
+  }
 }
 </style>
