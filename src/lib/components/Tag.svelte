9bf93b7e74442e6806493c2a3b2cb0bea65d45a4
szeroki <36236750+szerookii@users.noreply.github.com>
2023-02-07 16:35:24 +0100

    type

--- src/lib/components/Tag.svelte
+++ src/lib/components/Tag.svelte
@@ -3,8 +3,8 @@
     import type {IconDefinition} from "@fortawesome/free-brands-svg-icons";
 
     export let text: string;
-    export let icon: IconDefinition;
     export let href: string | undefined;
+    export let icon: IconDefinition;
 </script>
 
 <a href="{href}" class="gap-2 text-xs inline-flex font-bold px-3 text-white border-white border-2 py-1 rounded-full">
